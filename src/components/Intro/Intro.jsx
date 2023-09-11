cef3e64afbbdc222efb6eec72827e9383c0f7f90
TejaAndagundala <andagundalateja@gmail.com>
2023-09-11 12:11:57 +0530

    law-firm folders

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,24 +1,3 @@
-// const Intro = () => {
-//   return (
-//     <div className="flex justify-between p-24 text-white justify-center">
-//       <div className="w-[50%] pl-52 pr-24 ">
-//         <h2 className="text-[53px] font-semibold">Let’s Introduce Ourself</h2>
-//       </div>
-
-//       <div className="h-[206px] border border-[#6A6A6A]"></div>
-
-//       <div className="w-[50%] flex flex-col pl-40 gap-3 pr-20 ">
-//         <h2 className="text-[32px]">Criminal Lawyer</h2>
-//         <p className="text-white opacity-30 w-[450px]">
-//           Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-//           sint. Velit officia consequatduis enim velit mollit Exercitation.
-//         </p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Intro;
 import styles from "./Intro.module.css";
 const Intro = () => {
   return (
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,4 @@
 import logo from "../../assests/LogoImg.png";
-// const Navbar = () => {
-//   return (
-//     <div className="flex justify-between text-white">
-//       <div className="">
-//         <img src={logo} alt="law_firm_logo" />
-//       </div>
-//       <ul className="flex gap-24">
-//         <li>Home</li>
-//         <li>Attorneys</li>
-//         <li>Practice Areas</li>
-//         <li>About Us</li>
-//       </ul>
-//       <button className="text-white border border-2 p-2">Contact Now</button>
-//     </div>
-//   );
-// };
 import React from "react";
 import styles from "./Navbar.module.css";
 const Navbar = () => {
